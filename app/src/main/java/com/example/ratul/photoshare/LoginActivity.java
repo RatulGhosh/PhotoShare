30117b9b36ff7cb3c5bd766da85ca0cb83bd8f4e
RatulGhosh <ratulghoshr@gmail.com>
2015-10-20 01:05:19 +0530

    integrating facebook

---
+++ app/src/main/java/com/example/ratul/photoshare/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.ratul.photoshare;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Ratul on 10/19/2015.
+ */
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+
+    }
+}
